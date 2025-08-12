12d912157e04a243491520e7bfa7d5fd996c335a
wonmo <58138180+Gu-WonMo@users.noreply.github.com>
2025-08-12 15:12:23 +0900

    [section13] 감정 일기장 삭제, 수정 관련 레이아웃 추가

--- section13/src/pages/Edit.jsx
+++ section13/src/pages/Edit.jsx
@@ -1,9 +1,29 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Editor"
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
+
+  const onClickDelete = () => {
+    console.log(
+      window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요")
+    )
+  }
+
   return (
-    <div>{params.id}번 일기입니다.</div>
+    <div>
+      <Header 
+        title={"일기 수정하기"}
+        leftChild={<Button text={"< 뒤로가기"} onClick={()=>nav(-1)} />}  
+        rightChild={<Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />}  
+      />
+      <Editor 
+      
+      />
+    </div>
   )
 }
 
